218d5d256009c1f977275b0818d892e4116fdb33
Hans Ekbrand <hans@gpu-master>
2022-04-09 13:26:28 +0200

    autoadjust the threshold more agressively

--- src/mcts/auxengine.cc
+++ src/mcts/auxengine.cc
@@ -1199,13 +1199,18 @@ void Search::AuxWait() {
   // Adjust the Threshold so there is always work to do, aim at a 100 nodes left in the queue at move selection time.
   if(search_stats_->AuxEngineQueueSizeAtMoveSelectionTime < 100){
     // Get more work, decrease the Threshold
-    if(params_.GetAuxEngineVerbosity() >= 4) LOGFILE << "Queue of nodes to query is a bit low at move selection time, decreasing the threshold from " << search_stats_->AuxEngineThreshold << " to " << search_stats_->AuxEngineThreshold * 0.95 << " to increase the buffer." ;
-    search_stats_->AuxEngineThreshold = search_stats_->AuxEngineThreshold * 0.95;
+    if(params_.GetAuxEngineVerbosity() >= 4) LOGFILE << "Queue of nodes to query is a bit low at move selection time, decreasing the threshold from " << search_stats_->AuxEngineThreshold << " to " << search_stats_->AuxEngineThreshold * 0.90 << " to increase the buffer." ;
+    search_stats_->AuxEngineThreshold = search_stats_->AuxEngineThreshold * 0.90;
   }
-  if(search_stats_->AuxEngineQueueSizeAtMoveSelectionTime > 200){
+  if(search_stats_->AuxEngineQueueSizeAtMoveSelectionTime > 200 && search_stats_->AuxEngineQueueSizeAtMoveSelectionTime < 15000){
     // Get less work, increase the Threshold
-    if(params_.GetAuxEngineVerbosity() >= 4) LOGFILE << "Queue of nodes to query is a bit high at move selection time, increasing the threshold from " << search_stats_->AuxEngineThreshold << " to " << search_stats_->AuxEngineThreshold * 1.05 << " to decrease the buffer." ;
-    search_stats_->AuxEngineThreshold = search_stats_->AuxEngineThreshold * 1.05;
+    if(params_.GetAuxEngineVerbosity() >= 4) LOGFILE << "Queue of nodes to query is a bit high at move selection time, increasing the threshold from " << search_stats_->AuxEngineThreshold << " to " << search_stats_->AuxEngineThreshold * 1.10 << " to decrease the buffer." ;
+    search_stats_->AuxEngineThreshold = search_stats_->AuxEngineThreshold * 1.10;
+  }
+  if(search_stats_->AuxEngineQueueSizeAtMoveSelectionTime >= 15000){
+    // Get less work, increase the Threshold
+    if(params_.GetAuxEngineVerbosity() >= 4) LOGFILE << "Queue of nodes to query is a bit high at move selection time, increasing the threshold from " << search_stats_->AuxEngineThreshold << " to " << search_stats_->AuxEngineThreshold * 1.5 << " to decrease the buffer." ;
+    search_stats_->AuxEngineThreshold = search_stats_->AuxEngineThreshold * 1.5;
   }
 
   // purge obsolete nodes in the helper queues. Note that depending on the move of the opponent more nodes can become obsolete.
