9d0431256ca9e6436553a6cf420ff2c68df0441e
Hans Ekbrand <hans@gpu-master>
2022-04-13 23:14:48 +0200

    enqueue unconditionally if depth is low enough

--- src/mcts/search.cc
+++ src/mcts/search.cc
@@ -3131,19 +3131,17 @@ void SearchWorker::MaybeAdjustPolicyForHelperAddedNodes(const std::shared_ptr<Se
 	    // the move is promising
 	    if(strategy == "b") minimum_policy = d;
 	    if(strategy == "e") minimum_policy = std::min(0.90, minimum_policy * 1.2);
-
-	    // If the move is better than root, then enqueue it
-	    // if(factor_for_us * n->GetQ(0.0f) > root_q){
-	    // LOGFILE << "We are better than root. we: " << factor_for_us * n->GetQ(0.0f) << " root: " << root_q << " will enqueue it (unless the node is terminal).";
-	    // Check that it's not terminal and not already queued and not too deep.
-	    if(!n->IsTerminal() && n->GetAuxEngineMove() == 0xffff && depth < 30){
-	      AuxMaybeEnqueueNode(n);
-	    }
 	    
 	  } else {
 	    // Not promising, but since the helper recommended it, it is probably better than its policy, so give it some policy boosting.
 	    if(strategy == "e") minimum_policy = std::min(0.90, minimum_policy * 0.85);	    
 	  }
+
+	  // Check that it's not terminal and not already queued and not too deep.
+	  if(!n->IsTerminal() && n->GetAuxEngineMove() == 0xffff && depth < 30){
+	    AuxMaybeEnqueueNode(n);
+	  }
+
 	  search_->nodes_mutex_.unlock_shared();	  
 	}
 
