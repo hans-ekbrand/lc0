6f8a63e4b5dbc99c5c1d8a0351223ff037ead4d2
Hans Ekbrand <hans.ekbrand@gmail.com>
2024-05-06 01:34:51 +0200

    back to silent mode

--- src/selfplay/loop.cc
+++ src/selfplay/loop.cc
@@ -155,6 +155,7 @@ void Validate(const std::vector<V7TrainingData>& fileContents) {
     DataAssert(data.root_d >= 0.0f && data.root_d <= 1.0f);
     DataAssert(data.best_q >= -1.0f && data.best_q <= 1.0f);
     DataAssert(data.root_q >= -1.0f && data.root_q <= 1.0f);
+    // std::cout << "root_m: " << data.root_m << " best_m: " << data.best_m << " plies left: " << data.plies_left << " \n";
     DataAssert(data.root_m >= 0.0f);
     DataAssert(data.best_m >= 0.0f);
     DataAssert(data.plies_left >= 0.0f);
@@ -436,7 +437,10 @@ int ResultForData(const V7TrainingData& data) {
   }
   // Must be greater than 0, but having a simple if clause make the compiler warn about control reaches end of non-void function [-Wreturn-type]
   // if(data.result_q > 0){
-  if (! (data.result_q > 0)) throw Exception("Range Violation");
+  if (! (data.result_q > 0)) {
+    std::cout << "Strange Q value found: " << data.result_q << "\n";
+    throw Exception("Range Violation");
+  }
   return 1;
 }
 
@@ -479,8 +483,9 @@ void ProcessFile(const std::string& file, SyzygyTablebase* tablebase,
       while (reader.ReadChunk(&data)) {
         fileContents.push_back(data);
       }
-      std::cout << "number of chunks (moves) found: " << fileContents.size() << "\n";
+      // std::cout << "number of chunks (moves) found: " << fileContents.size() << "\n";
       Validate(fileContents);
+      // std::cout << "First check passed \n";      
       MoveList moves;
       for (int i = 1; i < fileContents.size(); i++) {
         moves.push_back(
@@ -490,7 +495,7 @@ void ProcessFile(const std::string& file, SyzygyTablebase* tablebase,
         // move so need to mirror them all to be applicable to apply to the
         // position before.
         moves.back().Mirror();
-	std::cout << "stored move " << moves.back().as_string() << ". ";
+	// std::cout << "stored move " << moves.back().as_string() << ". ";
       }
       Validate(fileContents, moves);
       games += 1;
@@ -727,7 +732,7 @@ void ProcessFile(const std::string& file, SyzygyTablebase* tablebase,
 	    // Only rescore if win/loss
             if (new_score != 0) {
               fileContents[i + 1].result_d = 0.0f;
-	      std::cout << "Rescoring at position: " << i + 1 << " from q=" << fileContents[i + 1].result_q << " to q=" << static_cast<float>(new_score) << ". " ;
+	      // std::cout << "Rescoring at position: " << i + 1 << " from q=" << fileContents[i + 1].result_q << " to q=" << static_cast<float>(new_score) << ". " ;
 	      fileContents[i + 1].result_q = static_cast<float>(new_score);
 	    }
           }
@@ -1127,17 +1132,17 @@ void ProcessFile(const std::string& file, SyzygyTablebase* tablebase,
       if (!outputDir.empty()) {
         std::string fileName = file.substr(file.find_last_of("/\\") + 1);
         TrainingDataWriter writer(outputDir + "/" + fileName);
-	std::cout << "Ready rescoring this game, now follows a list of q values: ";
+	// std::cout << "Ready rescoring this game, now follows a list of q values: ";
 	int i = 0;
         for (auto chunk : fileContents) {
           // Don't save chunks that just provide move history.
           if ((chunk.invariance_info & 64) == 0) {
-	    std::cout << "i:" << i << " q:" << chunk.result_q << " ";
+	    // std::cout << "i:" << i << " q:" << chunk.result_q << " ";
 	    i++;
             writer.WriteChunk(chunk);
           }
         }
-	std::cout << "\n";
+	// std::cout << "\n";
       }
 
       // Output data in Stockfish plain format.
